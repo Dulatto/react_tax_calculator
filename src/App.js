8d0d4605524789970988164d997cde51983b9613
Dulatto <dulat.omarov@gmail.com>
2021-12-16 22:41:27 -0500

    filters ui

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="row  bg-white py-4">
-          <h1 className="mt-4 pt-4">Income tax calculator Canada</h1>
+          <h1 className="mt-3 pt-4">Income tax calculator Canada</h1>
           <h5>Find out how much your salary is after tax</h5>
           <div className="col-sm-12">
             <Filters />
