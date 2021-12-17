57b8d6096929765ecb542ee0b7676533e3d8accc
Dulatto <dulat.omarov@gmail.com>
2021-12-16 22:17:28 -0500

    filters ui

--- src/Views/Filters.js
+++ src/Views/Filters.js
@@ -4,16 +4,19 @@ const Filters = () => {
     return (
         <div className="row mt-4">
             <div className="col-md-3">
-                <div class="mb-3">
-                    <label class="form-label">Enter your gross income</label>
-                    <input type="text" class="form-control" />
+                <div className="mb-3">
+                    <label className="form-label">Enter your gross income</label>
+                    <div className="input-group mb-3">
+                        <span className="input-group-text"><i className="fas fa-money-bill-wave"></i></span>
+                        <input type="text" className="form-control" />
+                    </div>
 
                 </div>
             </div>
             <div className="col-md-3">
-                <div class="mb-3">
-                    <label class="form-label">Per</label>
-                    <select class="form-select ">
+                <div className="mb-3">
+                    <label className="form-label">Per</label>
+                    <select className="form-select ">
                         <option>Small select</option>
                         <option>Small select</option>
                         <option>Small select</option>
@@ -22,9 +25,9 @@ const Filters = () => {
                 </div>
             </div>
             <div className="col-md-3">
-                <div class="mb-3">
-                    <label class="form-label">Where do you work?</label>
-                    <select class="form-select ">
+                <div className="mb-3">
+                    <label className="form-label">Where do you work?</label>
+                    <select className="form-select ">
                         <option>Small select</option>
                         <option>Small select</option>
                         <option>Small select</option>
@@ -33,8 +36,8 @@ const Filters = () => {
                 </div>
             </div>
             <div className="col-md-3 pt-2">
-                <div class="mt-4">
-                    <button type="button" class="btn btn-danger">Calculate</button>
+                <div className="mt-4">
+                    <button type="button" className="btn btn-danger">Calculate</button>
                 </div>
             </div>
         </div>
