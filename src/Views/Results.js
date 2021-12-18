f0f1d52e8624da50adadf63a9976a962a9ae2aa3
Dulatto <dulat.omarov@gmail.com>
2021-12-17 23:10:55 -0500

    salary ui

--- src/Views/Results.js
+++ src/Views/Results.js
@@ -5,8 +5,23 @@ const Results = () => {
         <div className="row bg-white mt-3 py-4">
             <h5>Withholding</h5>
             <hr />
-            <div className="col-md-7"></div>
-            <div className="col-md-5"></div>
+            <div className="col-md-6">
+                <div className="row">
+                    <div className="col-md-8 fw-bold">Salary</div>
+                    <div className="col-md-4">$26.67</div>
+                    <div className="col-md-8">Federal tax deduction</div>
+                    <div className="col-md-4">$26.67</div>
+                    <div className="col-md-8">Provincial tax deduction</div>
+                    <div className="col-md-4">$26.67</div>
+                    <div className="col-md-8">CPP deduction</div>
+                    <div className="col-md-4">$26.67</div>
+                    <div className="col-md-8">EI deduction</div>
+                    <div className="col-md-4 mb-2">$26.67</div>
+                    <hr />
+
+                </div>
+            </div>
+            <div className="col-md-6"></div>
         </div>
     );
 };
