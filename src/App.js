2ae9c7d8ed34ae7062e2901a1bb9fba20eeb7e98
Dulatto <dulat.omarov@gmail.com>
2021-12-17 22:25:48 -0500

    salary ui

--- src/App.js
+++ src/App.js
@@ -8,18 +8,17 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="row  bg-white py-4">
+        <div className="row bg-white py-4">
           <h1 className="mt-3 pt-4">Income tax calculator Canada</h1>
           <h5>Find out how much your salary is after tax</h5>
           <div className="col-sm-12">
             <Filters />
           </div>
         </div>
-        <div className="row">
-          <SalaryRate />
-          <Results />
-          <Summary />
-        </div>
+        <SalaryRate />
+        <Results />
+        <Summary />
+
       </div>
     </div>
   );
--- src/Views/SalaryRate.js
+++ src/Views/SalaryRate.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const SalaryRate = () => {
     return (
-        <div className="mt-5 bg-white ">
-            Salary
+        <div className="row mt-3 bg-white py-4">
+            <div className="col-md-2">Salary rate </div>
+            <div className="col-md-10">
+                <div className="row">
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
+                </div>
+            </div>
+
         </div>
     );
 };
