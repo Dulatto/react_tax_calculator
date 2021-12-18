c4bf1eb510a3693d4186102a7bf4f20d76fe9f31
Dulatto <dulat.omarov@gmail.com>
2021-12-17 22:55:08 -0500

    salary ui

--- src/Views/Results.js
+++ src/Views/Results.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Results = () => {
     return (
-        <div>
-            Rsults
+        <div className="row bg-white mt-3 py-4">
+            <h5>Withholding</h5>
+            <hr />
+            <div className="col-md-7"></div>
+            <div className="col-md-5"></div>
         </div>
     );
 };
--- src/Views/SalaryRate.js
+++ src/Views/SalaryRate.js
@@ -3,18 +3,17 @@ import React from 'react';
 const SalaryRate = () => {
     return (
         <div className="row mt-3 bg-white py-4">
-            <div className="col-md-2">Salary rate </div>
-            <div className="col-md-10">
+            <div className="col-md-3">Salary rate </div>
+            <div className="col-md-9">
                 <div className="row">
                     <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Month</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Biweekly</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Weekly</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Day</button></div>
+                    <div className="col-md-2"><button className="btn btn-light btn-sm">Hour</button></div>
                 </div>
             </div>
-
         </div>
     );
 };
