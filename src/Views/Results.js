67be8c054aab4f78634e497c10fd32b702cce8d5
Dulatto <dulat.omarov@gmail.com>
2021-12-17 23:20:02 -0500

    salary ui

--- src/Views/Results.js
+++ src/Views/Results.js
@@ -8,17 +8,28 @@ const Results = () => {
             <div className="col-md-6">
                 <div className="row">
                     <div className="col-md-8 fw-bold">Salary</div>
-                    <div className="col-md-4">$26.67</div>
+                    <div className="col-md-4 fw-bold text-end">$26.67</div>
                     <div className="col-md-8">Federal tax deduction</div>
-                    <div className="col-md-4">$26.67</div>
+                    <div className="col-md-4 text-end">$26.67</div>
                     <div className="col-md-8">Provincial tax deduction</div>
-                    <div className="col-md-4">$26.67</div>
+                    <div className="col-md-4 text-end">$26.67</div>
                     <div className="col-md-8">CPP deduction</div>
-                    <div className="col-md-4">$26.67</div>
+                    <div className="col-md-4 text-end">$26.67</div>
                     <div className="col-md-8">EI deduction</div>
-                    <div className="col-md-4 mb-2">$26.67</div>
+                    <div className="col-md-4 mb-2 text-end">$26.67</div>
                     <hr />
-
+                    <div className="col-md-8 fw-bold">Total tax</div>
+                    <div className="col-md-4 fw-bold text-end">$26.67</div>
+                    <div className="col-md-8 fw-bold">Net pay</div>
+                    <div className="col-md-4 fw-bold mb-2 text-end">$26.67</div>
+                    <hr />
+                    <div className="col-md-8">EI deduction</div>
+                    <div className="col-md-4 mb-2 text-end">$26.67</div>
+                    <hr />
+                    <div className="col-md-8">Marginal tax rate</div>
+                    <div className="col-md-4 text-end">$26.67</div>
+                    <div className="col-md-8">Average tax rate</div>
+                    <div className="col-md-4 mb-2 text-end">$26.67</div>
                 </div>
             </div>
             <div className="col-md-6"></div>
