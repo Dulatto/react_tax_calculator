c0158d975d8b8d2579a16b5977c865d3ecb34250
Dulatto <dulat.omarov@gmail.com>
2021-12-19 16:50:05 -0500

    states

--- src/Views/SalaryRate.js
+++ src/Views/SalaryRate.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { useStore } from '../store';
+import { per } from '../data';
 
 const SalaryRate = () => {
+    const [state, setState] = useStore();
+
+    let period = per.map((x) => {
+        return <div className="col-md-2"><button className="btn btn-light btn-sm" value={x.val}>{x.text}</button></div>
+    })
+
     return (
         <div className="row mt-3 bg-white py-4">
             <div className="col-md-3">Salary rate </div>
             <div className="col-md-9">
                 <div className="row">
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Annual</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Month</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Biweekly</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Weekly</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Day</button></div>
-                    <div className="col-md-2"><button className="btn btn-light btn-sm">Hour</button></div>
+                    {period}
                 </div>
             </div>
         </div>
