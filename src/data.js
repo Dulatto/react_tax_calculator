1b6f6961ca0bb0bc03f76e96e170b237b92c4ece
Dulatto <dulat.omarov@gmail.com>
2021-12-25 23:39:00 -0500

    calculate

--- src/data.js
+++ src/data.js
@@ -4,14 +4,11 @@ const provinces = [
     { val: 2, text: 'Manitoba' },
     { val: 3, text: 'New Brunswick' },
     { val: 4, text: 'Newfoundland and Labrador' },
-    { val: 5, text: 'Northwest Territories' },
-    { val: 6, text: 'Nova Scotia' },
-    { val: 7, text: 'Nunavut' },
-    { val: 8, text: 'Ontario' },
-    { val: 9, text: 'Prince Edward Island' },
-    { val: 10, text: 'Quebec' },
-    { val: 11, text: 'Saskatchewan' },
-    { val: 12, text: 'Yukon' },
+    { val: 5, text: 'Nova Scotia' },
+    { val: 6, text: 'Ontario' },
+    { val: 7, text: 'Prince Edward Island' },
+    { val: 8, text: 'Quebec' },
+    { val: 9, text: 'Saskatchewan' },
 ]
 
 const per = [
