c83a14d45af3b76651e480f80e891e58e4e4a271
Dulatto <dulat.omarov@gmail.com>
2021-12-29 22:35:55 -0500

    results

--- src/Views/OtherResults.js
+++ src/Views/OtherResults.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const OtherResults = () => {
     return (
-        <div className='row other-results'>
-            <div className='col-4 border-bottom'>Region</div>
-            <div className='col-4 border-bottom'>Net Pay</div>
-            <div className='col-4 border-bottom'>Average Tax</div>
+        <div className='row other-results mx-1'>
+            <div className='col-4 border-bottom fw-bold mb-1'>Region</div>
+            <div className='col-4 border-bottom fw-bold mb-1'>Net Pay</div>
+            <div className='col-4 border-bottom fw-bold mb-1'>Average Tax</div>
+            <div className='col-4 border-bottom mb-1'>Alberta</div>
+            <div className='col-4 border-bottom mb-1'></div>
+            <div className='col-4 border-bottom mb-1'></div>
         </div>
     );
 };
