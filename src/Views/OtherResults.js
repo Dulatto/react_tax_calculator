4a661069af725325c6512dc1b2e0ffa740480b8c
Dulatto <dulat.omarov@gmail.com>
2021-12-29 22:57:52 -0500

    results

--- src/Views/OtherResults.js
+++ src/Views/OtherResults.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { provincesList } from '../data';
+import { useStore } from '../store';
 
 const OtherResults = () => {
+    const [state, setState] = useStore();
+
+    let area = provincesList.map((x, index) => {
+        return (
+            <>
+                <div className='col-4 border-bottom mb-1' key={index}>{x.text}</div>
+                <div className='col-4 border-bottom mb-1' key={index}></div>
+                <div className='col-4 border-bottom mb-1' key={index}></div>
+            </>
+        )
+    })
+
     return (
         <div className='row other-results mx-1'>
             <div className='col-4 border-bottom fw-bold mb-1'>Region</div>
             <div className='col-4 border-bottom fw-bold mb-1'>Net Pay</div>
             <div className='col-4 border-bottom fw-bold mb-1'>Average Tax</div>
-            <div className='col-4 border-bottom mb-1'>Alberta</div>
-            <div className='col-4 border-bottom mb-1'></div>
-            <div className='col-4 border-bottom mb-1'></div>
+            {area}
         </div>
     );
 };
